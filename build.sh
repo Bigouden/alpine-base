4cada9aa0ecf4ced5ef0ada3b2987183ab3c45cb
Thomas GUIRRIEC <thomas@guirriec.fr>
2020-05-23 10:14:20 +0000

    Add Clean Step

--- build.sh
+++ build.sh
@@ -9,8 +9,9 @@ OS_ARCHITECTURE="x86_64"
 OS_DISTRIBUTION="alpine-minirootfs"
 OS_MAJOR_VERSION="3.11"
 OS_VERSION="${OS_MAJOR_VERSION}.6"
-ROOTFS_FILE=${OS_DISTRIBUTION}-${OS_VERSION}-${OS_ARCHITECTURE}.tar.gz
-SHA256_FILE=${OS_DISTRIBUTION}-${OS_VERSION}-${OS_ARCHITECTURE}.tar.gz.sha256
+COMPRESS_ROOTFS_FILE=${OS_DISTRIBUTION}-${OS_VERSION}-${OS_ARCHITECTURE}.tar.gz
+COMPRESS_SHA256_FILE=${OS_DISTRIBUTION}-${OS_VERSION}-${OS_ARCHITECTURE}.tar.gz.sha256
+ROOTFS_FILE=${OS_DISTRIBUTION}-${OS_VERSION}-${OS_ARCHITECTURE}.tar
 ROOTFS_URL=http://dl-cdn.alpinelinux.org/alpine/v${OS_MAJOR_VERSION}/releases/${OS_ARCHITECTURE}/${ROOTFS_FILE}
 SHA256_URL=http://dl-cdn.alpinelinux.org/alpine/v${OS_MAJOR_VERSION}/releases/${OS_ARCHITECTURE}/${SHA256_FILE}
 
@@ -26,6 +27,14 @@ checksum () {
     sha256sum -c -s ${1} || { RCODE=${?}; log "Failed to verify checksum for file : ${2}" ; exit ${RCODE}; }
     log "Checksum Control OK"
 }
+
+# Cleanup
+clean ()
+    log "Cleaning ..."
+    log "FILE : ${1}"
+    gunzip ${1}
+    tar -f ${2} --wildcards --delete ./lib/apk/db/*
+    log "Clean OK"
 
 # Downloading
 download () {
@@ -54,11 +63,14 @@ log () {
 ########
 
 # Download Files
-download ${ROOTFS_FILE} ${ROOTFS_URL}
-download ${SHA256_FILE} ${SHA256_URL}
+download ${COMPRESS_ROOTFS_FILE} ${ROOTFS_URL}
+download ${COMPRESS_SHA256_FILE} ${SHA256_URL}
 
 # Checksum Control
-checksum ${SHA256_FILE} ${ROOTFS_FILE}
+checksum ${COMPRESS_SHA256_FILE} ${COMPRESS_ROOTFS_FILE}
+
+# Clean RootFS
+clean ${COMPRESS_ROOTFS_FILE} ${ROOTFS_FILE}
 
 # Docker Import
 import ${ROOTFS_FILE} ${IMAGE}
